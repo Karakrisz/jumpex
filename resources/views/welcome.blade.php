1ee80400e6d26b7a358f149ade3e090b4979f6db
Boaz <webevolution13@gmail.com>
2024-01-23 19:00:25 +0100

    upDate
    front end

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -112,7 +112,16 @@
 
 
 <section>
-    partnerek
+    <div class="partners-content">
+        <h5 class="partners-content__h5 text-center">Partnereink</h5>
+        <div class="partners-content__iBox">
+            <img class="partners-content__iBox__img" src="/img/partners/hidral.svg" alt="jumpex">
+            <img class="partners-content__iBox__img" src="/img/partners/kleemann.svg" alt="jumpex">
+            <img class="partners-content__iBox__img" src="/img/partners/orona.svg" alt="jumpex">
+            <img class="partners-content__iBox__img" src="/img/partners/otis.svg" alt="jumpex">
+            <img class="partners-content__iBox__img" src="/img/partners/schindler.svg" alt="jumpex">
+        </div>
+    </div>
 </section>
 
 
